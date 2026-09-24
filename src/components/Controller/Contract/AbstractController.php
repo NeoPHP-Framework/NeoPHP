290b2a6c69dc39ef14c8a4f4ea3dc3a2bde4e229
BenjiLeLoustik <boezio.benjamin.pro@outlook.com>
2026-09-24 21:27:14 +0200

    feat(controller): add dispatch() to AbstractController

--- src/components/Controller/Contract/AbstractController.php
+++ src/components/Controller/Contract/AbstractController.php
@@ -6,6 +6,7 @@ namespace NeoPHP\Component\Controller\Contract;
 
 use NeoPHP\Component\Container\Helper\Controller\ContainerController;
 use NeoPHP\Component\Cookie\Helper\Controller\CookieController;
+use NeoPHP\Component\Event\Helper\Controller\EventController;
 use NeoPHP\Component\Flash\Helper\Controller\FlashController;
 use NeoPHP\Component\Http\Helper\Controller\HttpController;
 use NeoPHP\Component\Routing\Helper\Controller\RoutingController;
@@ -16,6 +17,7 @@ abstract class AbstractController implements ControllerInterface
 {
     use ContainerController;
     use CookieController;
+    use EventController;
     use FlashController;
     use HttpController;
     use RoutingController;
