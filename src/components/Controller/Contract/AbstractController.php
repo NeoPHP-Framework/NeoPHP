a387cd63e00c87575b1a97f06cb2e6f16c835758
BenjiLeLoustik <boezio.benjamin.pro@outlook.com>
2026-09-25 06:31:15 +0200

    feat(database): add getConnection() controller trait

--- src/components/Controller/Contract/AbstractController.php
+++ src/components/Controller/Contract/AbstractController.php
@@ -6,6 +6,7 @@ namespace NeoPHP\Component\Controller\Contract;
 
 use NeoPHP\Component\Container\Helper\Controller\ContainerController;
 use NeoPHP\Component\Cookie\Helper\Controller\CookieController;
+use NeoPHP\Component\Database\Helper\Controller\DatabaseController;
 use NeoPHP\Component\Event\Helper\Controller\EventController;
 use NeoPHP\Component\Flash\Helper\Controller\FlashController;
 use NeoPHP\Component\Http\Helper\Controller\HttpController;
@@ -18,6 +19,7 @@ abstract class AbstractController implements ControllerInterface
 {
     use ContainerController;
     use CookieController;
+    use DatabaseController;
     use EventController;
     use FlashController;
     use HttpController;
